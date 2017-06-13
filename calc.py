f9dc7ab75b2e171f5fef74eb78f604f1d4587966
Edoardo Zatterin <edoardo.zatterin@esrf.fr>
2017-06-13 11:07:07 +0200

    major reorg

---
+++ calc.py
@@ -0,0 +1,134 @@
+"""
+Set of functions to simulate CTRs and RSMs. Updated 7.11.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pkg_resources import resource_filename
+from edoxrd.utils import asf, mat_dict, tt2q, find_osc
+from edoxrd.read import read_data, read_rsm_data
+
+def calc_str_fac(material,ttheta):
+
+	"""
+	Calculate the Structure Factor of a material belonging to the Perovskite
+	Oxides family. Materials: STO,PTO,MTO,SRO,KTO,DSO.
+
+	Parameters
+	----------
+	material: string
+	ttheta: ndarray
+
+	Returns
+	-------
+	F: ndarray
+
+	Example
+	-------
+	>>> F = calc_str_fac('PTO',xdata)
+
+	"""
+
+	# Initialise
+	mat_param, mat_pos = mat_dict()
+	q = tt2q(ttheta)
+	f = asf(q)
+
+	ls = dict(
+	    STO = (['Sr', 'Ti'] + ['O']*3),
+	    PTO = (['Pb', 'Ti'] + ['O']*3),
+	    MTO = (['Mn', 'Ti'] + ['O']*3),
+	    SRO = (['Sr', 'Ti'] + ['O']*3),
+	    KTO = (['K', 'Ta'] + ['O']*3),
+	    DSO = (['Dy', 'Sc'] + ['O']*3)
+	        )
+
+	if material == 'SRO_b' or material == 'SRO_t':
+	    atom_list = ls['SRO']
+	else:
+	    atom_list = ls[material]
+
+	F = 0
+	for i, atom in enumerate(atom_list):
+	    F += f[atom] * np.exp(1j * ( q * mat_pos[material][i,2]*mat_param[material][2]))
+
+	return F
+
+def calc_thickness(fname, datadir, threshold=1e-4, distance=20, side='r',
+					comm='*'):
+
+	"""
+	Calculate the thickness of a thin film on top of a substrate from its
+	Laue oscillations.
+
+	Parameters
+	----------
+	fname: string
+		The filename in .ras format.
+	datadir: string
+		The directory containing fname.
+	thres : float between [0., 1.]
+	    Normalized threshold. Only the peaks with amplitude higher than the
+	    threshold will be detected. Default: 1e-4
+	min_dist : int
+	    Minimum distance between each detected peak. The peak with the highest
+	    amplitude is preferred to satisfy this constraint. Default: 20.
+	side: string
+		Side of the film peak where to look for oscillations. Either 'l'
+		(left), or 'r' (right). Default: 'r'
+
+	Returns
+	-------
+	t: float
+		Thickness of the film.
+
+	Example
+	-------
+	>>> t = calc_thickness(dset, d, threshold=1e-5, distance=15, side='l')
+
+	"""
+
+	xpeaks, ypeaks = find_osc(fname, datadir, threshold=threshold,
+							  m_distance=distance, peak_side=side, comm=comm)
+	x = np.asarray([x for x in range(len(xpeaks))]) # oscillation peak order
+	y = 4 * np.pi * np.sin(np.deg2rad(xpeaks/2)) / 0.15406 # q_m's
+	m, b = np.polyfit(x, y, 1) # linear fit
+	t = abs(2 * np.pi / m)
+	fig, ax = plt.subplots(1,2, figsize=(12,4))
+	fig.suptitle('The thickness of sample {0} is {1:.3f} nm with accuracy {2:.3f}'\
+	      .format(fname[:6], t, 1./x.max() ))
+	ax[0].scatter(xpeaks, ypeaks,c='red')
+	ax[0].plot(*read_data(fname, datadir, comments=comm)); ax[0].set_yscale('log')
+	ax[1].scatter(x, y)
+	ax[1].plot(x, x*m+b, c='red', label='$q_m = {0:.3f}m + {1:.3f}$'.format(m, b))
+	ax[1].legend(); plt.show()
+	return t
+
+def l_prof_max(filelist, d, L=None, q=False):
+
+	""" TODO! """
+
+	# select correct dataset
+	pto = filelist[1]
+
+	# read data
+	h, l, i = read_rsm_data(pto, d, scale='lin')
+
+	if L==None:
+	    L = l[np.where(i==i.max())]
+
+	# find indexes of values close to selected L
+	a = abs(l-L)
+	mask_a = ((a > 0) & (a < 1e-4))
+	lst_a = []
+	for number in a[mask_a]:
+	    lst_a.append([(int(np.where(a==number)[0])),int(np.where(a==number)[1])])
+	idx_l = np.array(lst_a)
+
+	I = i[idx_l[:,0], idx_l[:,1]][::-1]
+	H = h[idx_l[:,0], idx_l[:,1]][::-1]
+
+	if q == True:
+	    H = (2*np.pi/0.399)*(H +1)
+
+	return H, I
---
+++ ctr.py
@@ -0,0 +1,170 @@
+import numpy as np
+from pkg_resources import resource_filename
+from edoxrd.utils import asf, mat_dict, tt2q
+from edoxrd.read import read_data
+from edoxrd.calc import calc_str_fac
+
+def calc_ctr(fname, d, sub, film, Nfilm, c_film, Nelectrode_b=0, Nelectrode_t=0,
+            c_electrode_b=0, c_electrode_t=0, scale=1e7, Nsub=1e4, comm='*'):
+
+	"""
+	Calculate the thickness of a thin film on top of a substrate from its
+	Laue oscillations.
+
+	Parameters
+	----------
+	fname: string
+		The filename in .asc format.
+	datadir: string
+		The directory containing fname.
+	sub: string
+        The name of the substrate material.
+    film: string
+        The name of the film material
+    Nfilm: int
+        The thickness of the film in unit cells.
+    c_film: float
+        The out-of-plane lattice parameter of the film in A.
+    Nelectrode_b: int
+        The thickness of the Bottom electrode (SRO_b) in unit cells.
+        Default: 0 (No bottom Electrode).
+    Nelectrode_t: int
+        The thickness of the Top electrode (SRO_t) in unit cells.
+        Default: 0 (No top Electrode).
+    c_electrode_b: float
+        The out-of-plane lattice parameter of the Bottom electrode in A.
+        Default: 0 (No bottom Electrode).
+    c_electrode_t: float
+        The out-of-plane lattice parameter of the Top electrode in A.
+        Default: 0 (No top Electrode).
+    scale: int
+        Overall multiplier for the calculated intensity.
+        Default: 1e7.
+    Nsub: int
+        Thickness of the Substrate layer.
+        Default: 1e4.
+
+	Returns
+	-------
+	xdata: ndarray
+        The original measured twotheta range.
+    I: ndarray
+        The calculated Intensity.
+
+	Example
+	-------
+	>>> x, y = xrd.calc_ctr('e16014_09_t2t_002.ras', d, 'KTO', 'PTO', 60, 3.92)
+
+	"""
+
+	# Constants
+	wave = 1.5406
+	mu = 4e4
+	elec_b = 'SRO_b'
+	elec_t = 'SRO_t'
+	param, pos = mat_dict()
+	c_sub = param[sub][2]
+
+	# Read the data
+	xdata, ydata = read_data(fname, d, comments=comm)
+	q = tt2q(xdata)
+
+	# No electrodes
+	if Nelectrode_b <=0 and Nelectrode_t <= 0:
+
+		# Load data
+		mats = [sub, film]
+		t = (Nsub*c_sub + Nfilm*c_film)
+
+		# Calc structure factor and build shape function dictionary
+		F = {}
+		for material in mats:
+			F[material] = calc_str_fac(material, xdata) # does not want q, fix?
+			S = {sub:0, film:0}
+		# Calc shape function for each material
+		for l in range(0, int(Nsub)):
+			S[sub] += np.exp((t-l*c_sub) * (1j*q-(4*np.pi)/(mu*wave*q)))
+		for l in range(0, int(Nfilm)):
+		    S[film] += np.exp((t - (l*c_film + Nsub*c_sub)) *
+			(1j*q - (4*np.pi) / (mu*wave*q)))
+
+	# Top electrode
+	elif Nelectrode_b <= 0 and Nelectrode_t > 0:
+
+		# Load data
+		mats = [sub, elec_t, film]
+		t = (Nsub*c_sub + Nelectrode_t*c_electrode_t + Nfilm*c_film)
+
+		# Calc structure factor and build shape function dictionary
+		F = {}
+		for material in mats:
+		    F[material] = calc_str_fac(material, xdata)
+		    S = {sub:0, film:0, elec_t:0}
+
+		# ?
+		for l in range(0,int(Nsub)):
+		    S[sub] += np.exp((t-l*c_sub) * (1j*q-(4*np.pi)/(mu*wave*q)))
+		for l in range(0, int(Nfilm)):
+		    S[film] += np.exp((t - (l*c_film + Nsub*c_sub)) *
+			(1j*q - (4*np.pi) / (mu*wave*q)))
+		for l in range(0, int(Nelectrode_t)):
+		    S[elec_t] += np.exp((t - (l*c_electrode_t + Nfilm*c_film
+			+ Nsub*c_sub)) * (1j*q - (4*np.pi) / (mu*wave*q)))
+
+	# Bottom electrode
+	elif Nelectrode_b > 0 and Nelectrode_t <= 0:
+
+		mats = [sub, elec_b, film]
+		t = (Nsub*c_sub + Nelectrode_b*c_electrode_b + Nfilm*c_film)
+
+		F = {}
+		for material in mats:
+		    F[material] = calc_str_fac(material, xdata)
+		    S = {sub:0, film:0, elec_b:0}
+
+		# ?
+		for l in range(0,int(Nsub)):
+		    S[sub] += np.exp((t-l*c_sub) * (1j*q-(4*np.pi)/(mu*wave*q)))
+		for l in range(0, int(Nelectrode_b)):
+		    S[elec_b] += np.exp((t - (l*c_electrode_b + Nsub*c_sub)) *
+		    (1j*q - (4*np.pi) / (mu*wave*q)))
+		for l in range(0, int(Nfilm)):
+		    S[film] += np.exp((t - (l*c_film + Nelectrode_b*c_electrode_b
+		    + Nsub*c_sub)) * (1j*q - (4*np.pi) / (mu*wave*q)))
+
+	# Both electrodes
+	elif Nelectrode_b > 0 and Nelectrode_t > 0:
+
+		mats = [sub,film,elec_b,elec_t]
+		t = (Nsub*c_sub + Nelectrode_b*c_electrode_b + Nfilm*c_film +
+		 	 Nelectrode_t*c_electrode_t)
+
+		F = {}
+		for material in mats:
+		    F[material] = calc_str_fac(material, xdata)
+		    S = {sub:0, film:0, elec_b:0, elec_t:0}
+
+		# ?
+		for l in range(0,int(Nsub)):
+		    S[sub] += np.exp((t-l*c_sub) * (1j*q-(4*np.pi)/(mu*wave*q)))
+		for l in range(0, int(Nelectrode_b)):
+		    S[elec_b] += np.exp((t - (l*c_electrode_b + Nsub*c_sub)) *
+		    (1j*q - (4*np.pi) / (mu*wave*q)))
+		for l in range(0, int(Nfilm)):
+		    S[film] += np.exp((t - (l*c_film + Nelectrode_b*c_electrode_b
+		    + Nsub*c_sub)) * (1j*q - (4*np.pi) / (mu*wave*q)))
+		for l in range(0, int(Nelectrode_t)):
+		    S[elec_t] += np.exp((t - (l*c_electrode_t + Nfilm*c_film +
+		    Nelectrode_b*c_electrode_b + Nsub*c_sub)) *
+		    (1j*q - (4*np.pi) / (mu*wave*q)))
+
+
+	g = 0
+	for material in mats:
+		g += F[material]*S[material]
+
+	# Normalisation and Intensity
+	g = g / g.max()
+	I = scale * g * np.conj(g)
+
+	return xdata, I
